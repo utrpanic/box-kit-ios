62e8312c0fb86fb39c644124031cb0d03ec85bbf
BoxJeon <utrpanic@gmail.com>
2019-04-13 00:14:36 +0900

    Log에 public 누락.

--- BoxJeonExtension/Log.swift
+++ BoxJeonExtension/Log.swift
@@ -6,13 +6,13 @@
 //  Copyright © 2019 boxjeon. All rights reserved.
 //
 
-class Log {
+public class Log {
     
-    class func d<T>(_ value: T, file: NSString = #file, line: Int = #line) {
+    static func d<T>(_ value: T, file: NSString = #file, line: Int = #line) {
         print("\(file.lastPathComponent.ns.deletingPathExtension)[\(line)]: \(value)", terminator: "\n")
     }
     
-    class func e<T>(_ value: T, file: NSString = #file, line: Int = #line) {
+    static func e<T>(_ value: T, file: NSString = #file, line: Int = #line) {
         print("\(file.lastPathComponent.ns.deletingPathExtension)[\(line)]: Error - \(value)", terminator: "\n")
     }
 }
