53a158f0238091036f04dd9a211ee482c332cb26
BoxJeon <utrpanic@gmail.com>
2019-08-10 22:08:26 +0900

    Semantic Version으로 변경. 반드시 major.minor.patch를 지켜야함.

--- Package.swift
+++ Package.swift
@@ -1,18 +1,13 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "BoxKit",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "BoxKit",
             targets: ["BoxKit"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/Flight-School/AnyCodable", from: "0.2.2"),
     ],
     targets: [
